4f951e3071347a54ac29070c30b1c724925cf53f
RodinaMohamed645 <rodinamohammed062@gmail.com>
2025-12-31 13:17:56 +0200

    Add other component

---
+++ src/app/api-error/api-error.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-error',
+  imports: [],
+  templateUrl: './api-error.html',
+  styleUrl: './api-error.css',
+})
+export class ApiError {
+
+}
